b9dae7de94f486b1490a5df11b374ff2d768c17c
Tapan Prajapati <tapanprajapati22@gmail.com>
2021-02-05 19:13:36 +0530

    Initial Commit

---
+++ app/src/main/java/com/wmt/esparkbizpractical/activity/AddCountryActivity.java
@@ -0,0 +1,64 @@
+package com.wmt.esparkbizpractical.activity;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.wmt.esparkbizpractical.MainActivity;
+import com.wmt.esparkbizpractical.R;
+import com.wmt.esparkbizpractical.sqLite.DBManager;
+
+import java.util.Calendar;
+
+public class AddCountryActivity extends Activity implements View.OnClickListener {
+
+    private Button addTodoBtn;
+    private EditText subjectEditText;
+    private EditText descEditText;
+
+    private DBManager dbManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Add Record");
+
+        setContentView(R.layout.activity_add_record);
+
+        subjectEditText = (EditText) findViewById(R.id.subject_edittext);
+        descEditText = (EditText) findViewById(R.id.description_edittext);
+
+        addTodoBtn = (Button) findViewById(R.id.add_record);
+
+        dbManager = new DBManager(this);
+        dbManager.open();
+        addTodoBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_record:
+
+                final String name = subjectEditText.getText().toString();
+                final String desc = descEditText.getText().toString();
+
+                dbManager.insert(name, desc);
+
+                Intent main = new Intent(AddCountryActivity.this, MainActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                startActivity(main);
+                break;
+        }
+    }
+}
